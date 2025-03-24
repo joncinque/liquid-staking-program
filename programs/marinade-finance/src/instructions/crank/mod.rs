b50cd89877dd9d04a553a527be999fbfa77d448e
Jon C <me@jonc.dev>
2025-03-24 18:28:24 +0100

    program: Add canonical PDA for stakes per validator
    #### Problem
    
    The mSOL program allows for many different stake accounts delegated to a
    single validator, which is very flexible, but can make accounting more
    difficult, since there's no one place for a particular validator's
    stake.
    
    #### Summary of changes
    
    Following a model similar to spl-stake-pool, enforce that all stake
    eventually converges to a PDA based on the pool and validator pubkeys.
    
    * Introduce a new instruction to create the PDA based on a stake account
      delegated to a particular validator, by splitting everything into the
      target account
    * Abort merge-stake if the destination is not the PDA
    
    After this change, before attempting a merge, bots must check if the
    canonical PDA exists for the validator, and add a `CreateCanonicalStake`
    instruction. And then all merges must go into the PDA.
    
    Other than that, the changes are minimal.

--- programs/marinade-finance/src/instructions/crank/mod.rs
+++ programs/marinade-finance/src/instructions/crank/mod.rs
@@ -1,9 +1,11 @@
+pub mod create_canonical_stake;
 pub mod deactivate_stake;
 pub mod merge_stakes;
 pub mod redelegate;
 pub mod stake_reserve;
 pub mod update;
 
+pub use create_canonical_stake::*;
 pub use deactivate_stake::*;
 pub use merge_stakes::*;
 pub use redelegate::*;
--- programs/marinade-finance/src/lib.rs
+++ programs/marinade-finance/src/lib.rs
@@ -224,6 +224,16 @@ pub mod marinade_finance {
             .process(destination_stake_index, source_stake_index, validator_index)
     }
 
+    pub fn create_canonical_stake(
+        ctx: Context<CreateCanonicalStake>,
+        source_stake_index: u32,
+        validator_index: u32,
+    ) -> Result<()> {
+        check_context(&ctx)?;
+        ctx.accounts
+            .process(source_stake_index, validator_index)
+    }
+
     pub fn redelegate(
         ctx: Context<ReDelegate>,
         stake_index: u32,
